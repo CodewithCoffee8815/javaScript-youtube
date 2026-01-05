00a57de3ef29f89b3d8a7a0202697240dafb7ae1
CodewithCoffee8815 <shahanawazkhnn@gmail.com>
2026-01-05 10:12:37 +0000

    datatype

---
+++ basics/2datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js as a new version
+
+//alert(3 + 3); // we are using nodejs, not a browser
+
+
+
+// primitive datatypes
+
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefiend => 
+// symbol => unique
+
+// object
+
+
