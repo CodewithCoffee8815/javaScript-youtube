1d700c9b1e696f2c0bd0969a8cab8f04da00d393
CodewithCoffee8815 <shahanawazkhnn@gmail.com>
2026-01-05 18:21:55 +0000

    conversion

--- basics/3conversionOperation.js
+++ basics/3conversionOperation.js
@@ -30,3 +30,57 @@ console.log(anyNumber);
 
 console.log(newNumber);
 console.log(typeof newNumber);
+
+
+// *******************************************
+
+let value = 3;
+ let negValue = -value;
+
+ console.log( negValue );
+ 
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2/3);
+ console.log(2%3); // for reminder very imp for other code
+ console.log(2**3); // 2 to the power 3
+ 
+
+ let str ="Hello";
+ let str1 = " Shahanawaz";
+
+ let concat = str + str1;
+
+ console.log(concat);
+ 
+ console.log(1 + "2");
+ console.log("1" + 2);
+ console.log("1" + 2 + 2);
+ console.log(1 + 2 + "2");
+
+ console.log(+true); //its chane it into booleans to number 
+
+ console.log(+""); // its also convert into number
+
+ let num1, num2, num3 ;
+
+ num1 = num2 = num3 = 2+2;
+
+ console.log(num1);
+ console.log(num2);
+ console.log(num3);
+ console.log();
+ 
+
+ let gameCounter = 100;
+
+ console.log(gameCounter);
+
+ console.log(gameCounter++);
+
+//  console.log(++gameCounter);
+ 
+ 
+ 
+ 
