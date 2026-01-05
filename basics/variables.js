0dc339e1abff733deb1ef0f14836d664ca72782d
CodewithCoffee8815 <shahanawazkhnn@gmail.com>
2026-01-05 06:45:36 +0000

    variables in js

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 77464;
+let accountEmail = "shah@gmail.com";
+var accountPassword = "12345";
+
+accountCity = "Bhopal";
+
+// accountId = 41414; // Not Allowed
+accountEmail = "abc@gmail.com"
+accountPassword = "88733";
+accountCity = "Indore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+/*
+ prefer to not use var variable 
+because of block scope and fucnctional scope issue
+*/
