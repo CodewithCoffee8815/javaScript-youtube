a27cca32089924b3ce52b08f983cf95f27396479
CodewithCoffee8815 <shahanawazkhnn@gmail.com>
2026-01-06 07:35:04 +0000

    Comparision

---
+++ basics/4comparision.js
@@ -0,0 +1,34 @@
+console.log(2<3);
+console.log(2>3);
+console.log(2==2);
+console.log(2!=3);
+console.log(2>=3);
+console.log(2<=3);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log("Null comparision");
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null < 0);
+console.log(null > 0);
+
+console.log("Undefined comparision");
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+console.log("Strict check");
+
+console.log("2" == 2);
+console.log("2" === 2);
+console.log("2" != 2);
+console.log("2" !== 2);
+
